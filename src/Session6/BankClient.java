0af4368789b52a63f87289086d8e7d3d6cff4296
Bryan Garay <bryangarayacademico@gmail.com>
2023-10-17 18:54:18 -0500

    Bank Git homework

---
+++ src/Session6/BankClient.java
@@ -0,0 +1,19 @@
+package Session6;
+
+public class BankClient {
+    private User user;
+    private BankAccount account;
+
+    public BankClient(User user, BankAccount account) {
+        this.user = user;
+        this.account = account;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public BankAccount getAccount() {
+        return account;
+    }
+}
